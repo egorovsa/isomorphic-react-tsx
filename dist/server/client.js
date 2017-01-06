748f46e4c0d7ea77daf04bb4198412a0b60138e3
Егоров Сергей <egorovsa2@gmail.com>
2017-01-06 10:49:14 +0200

    remove dist from repo

--- dist/server/client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-var react_router_1 = require("react-router");
-var router_1 = require("./router");
-window.onload = function () {
-    ReactDOM.render(React.createElement(react_router_1.Router, { history: react_router_1.browserHistory }, router_1.default), document.getElementById('app'));
-};
